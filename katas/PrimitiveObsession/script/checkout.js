18b3743f9d76624384f05ec8685898b6360252b1
Katie Roberts <katie.roberts@bbc.co.uk>
2014-09-08 15:08:25 +0100

    a failing test for you

--- katas/PrimitiveObsession/script/checkout.js
+++ katas/PrimitiveObsession/script/checkout.js
@@ -1,5 +1,8 @@
 function Checkout() {
     this.scan = function(item) {
         console.log('I have scanned: ' + item);
-    }
+    },
+	this.total = function() {
+		return 0;
+	}
 }
--- katas/PrimitiveObsession/test/checkout.spec.js
+++ katas/PrimitiveObsession/test/checkout.spec.js
@@ -1,12 +1,17 @@
-describe('Primitive Obsession', function() {
-    var checkout
+describe ('Primitive Obsession', function () {
+	var checkout
 
-    beforeEach(function() {
-        checkout = new Checkout()
-    })
+	beforeEach (function () {
+		checkout = new Checkout ()
+	});
 
-    it('do nothing new', function() {
-        checkout.scan('an item')
-        expect(1).not.toBe(2)
-    })
+	it ('do nothing new', function () {
+		checkout.scan ('an item');
+		expect (1).not.toBe (2)
+	});
+
+	it ('scans an apple at the correct price', function (){
+		checkout.scan('apple');
+		expect(checkout.total).toBe(30);
+	});
 })
